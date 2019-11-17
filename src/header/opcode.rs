8ba709a7ec0a2cc48622064f6ba264dace58f60a
Ollie Nye <ollie@nyemail.co.uk>
2019-11-17 15:05:11 +0000

    Add header

---
+++ src/header/opcode.rs
@@ -0,0 +1,5 @@
+// Spotted
+
+pub const OP_SPOT_IMAGE: u16 = 0x0001;
+pub const OP_SPOT_CONTROL: u16 = 0x0002;
+pub const OP_SPOT_COORDINATE: u16 = 0x0003;
---
+++ src/header/spotted_header.rs
@@ -0,0 +1,35 @@
+pub struct SpottedHeader {
+  id: [u8; 8],
+  opcode: u16,
+  protocol_version: u16
+}
+
+impl SpottedHeader {
+  pub fn new(opcode: u16) -> SpottedHeader {
+
+    let mut id: [u8; 8] = id();
+
+    let protocol_version: u16 = 0x0001;
+
+    let header = SpottedHeader { id, opcode, protocol_version };
+
+    header
+  }
+
+  pub fn serialize(&self) -> Vec<u8> {
+    let mut out = Vec::new();
+    out.extend(self.id.iter().cloned());
+    out.extend(self.opcode.to_be_bytes().iter().cloned());
+    out.extend(self.protocol_version.to_be_bytes().iter().cloned());
+
+    out
+  }
+}
+
+pub fn id() -> [u8; 8] {
+  let mut id: [u8; 8] = [0x00; 8];
+
+  id.copy_from_slice("Spotted\0".as_bytes());
+
+  id
+}
