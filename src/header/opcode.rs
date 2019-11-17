3948f6ca07def55ca049290ffd7bd6392408b797
Ollie Nye <ollie@nyemail.co.uk>
2019-11-17 16:22:21 +0000

    Update opcode names

--- src/header/opcode.rs
+++ src/header/opcode.rs
@@ -1,5 +1,5 @@
 // Spotted
 
-pub const OP_SPOT_IMAGE: u16 = 0x0001;
-pub const OP_SPOT_CONTROL: u16 = 0x0002;
-pub const OP_SPOT_COORDINATE: u16 = 0x0003;
+pub const OP_IMAGE: u16 = 0x0001;
+pub const OP_CONTROL: u16 = 0x0002;
+pub const OP_COORDINATE: u16 = 0x0003;
